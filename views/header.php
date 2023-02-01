2abd156fde52f4054c210f59d3f8bb049d36d754
Alexandru Miron <60828762+mironalex00@users.noreply.github.com>
2023-02-01 17:18:50 +0100

    Añadidos iconos FontAwesome

--- views/header.php
+++ views/header.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://ka-f.fontawesome.com/releases/v6.2.0/css/free-v4-font-face.min.css">
+    <link rel="stylesheet" href="https://ka-f.fontawesome.com/releases/v6.2.0/css/free-v5-font-face.min.css">
+    <link rel="stylesheet" href="https://ka-f.fontawesome.com/releases/v6.2.0/css/free-v4-shims.min.css">
+    <link rel="stylesheet" href="https://ka-f.fontawesome.com/releases/v6.2.0/css/free.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css" />
     <link rel="stylesheet" href="./assets/css/main.css">
     <script src="./assets/js/main.js"></script>
     <title>Servetus</title>
