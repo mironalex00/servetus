8d7e576d61853646a5010dc81711068bcf859aa5
Alexandru Miron <60828762+mironalex00@users.noreply.github.com>
2023-02-09 01:35:39 +0100

    Terminado de comentar el código en index.php y solucionado un bug

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
     include('./views/header.php');
     #   OBTENEMOS LA ULTIMA PARTE DE LA URL, ESTA INDICARÁ QUE RECURSO VAMOS A PEDIR
     $baseUrl = substr($_SERVER['REQUEST_URI'], strlen(preg_replace("/\/(?:.(?!\/))+$/im", '', $_SERVER['SCRIPT_NAME'])) + 1);
+    #   SI CONTIENE SLASH AL FINAL DE LA RUTA
+    if(str_ends_with($baseUrl, '/')){
+        #   SE SOBREESCRIBE ELIMINANDO ESTE ULTIMO CARÁCTER
+        $baseUrl = preg_replace("/\/(?!\/)+$/im", '', $baseUrl);
+    }
     #   SE GUARDA EN VARIABLE DIRECTORIO DE VISTAS
     $views = __DIR__ . DIRECTORY_SEPARATOR . 'views'. DIRECTORY_SEPARATOR;
     #   FILTRO QUE PASARÁ LOS ARCHIVOS
@@ -17,13 +22,13 @@
     foreach(new RecursiveIteratorIterator($recDirIterator) as $file){
         #   SI ES DIRECTORIO SE PASA
         if(is_dir($file)) continue;
-        #
+        #   SI ES INDEX SE SALE
         if(strlen($baseUrl) === 0) break;
         #   SI NO ESTÁ DENTRO DEL FILTRO
         if (!in_array(strtolower( $file->getBasename('.php') ), $skip)){
-            #   
+            #   SE GUARDA LA PATH
             $basename = $file->getRealPath();
-            #
+            #   SE REEMPLAZAN CON BACKSLASH
             $expected = preg_replace("~\\/~mD", '\\', $baseUrl) . '.php';
             #   SI EL BASENAME DEL ARCHIVO PHP RECORRIDO COINCIDE
             if(str_ends_with($basename, $expected)){
@@ -37,5 +42,5 @@
     #   SE REQUIERE EL ARCHIVO
     require_once($required);
     #   SE INCLUYE EL FOOTER
-    include('./views/footer.php')
+    include('./views/footer.php');
 ?>
